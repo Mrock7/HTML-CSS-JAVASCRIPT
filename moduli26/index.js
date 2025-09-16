c6f90ed16ec1f93d0d6a7d2b5ce4de6f0defce98
Mrock7 <guri.morina1@gmail.com>
2025-09-16 20:42:06 +0200

    n

---
+++ moduli26/index.js
@@ -0,0 +1,88 @@
+console.log("goodbye!");
+
+var x1 = "digital school";
+
+
+var a = 50+50;
+
+console.log(a);
+
+var b = 60;
+
+var c = 80;
+
+var d = b+c;
+
+console.log(d);
+
+var e  = 40;
+
+var f = 10;
+
+var g=e-f;
+
+console.log(g);
+
+var h=100;
+var i=5;
+var j=h/i;
+
+console.log(j);
+
+var k = 10;
+var l = 4; 
+var m=k%l;
+
+console.log(m);
+
+
+var n = 99;
+n++;
+console.log(n)
+
+var o = 101;
+n--
+console.log(o);
+
+
+var p = 20;
+var q = p**2;
+console.log(q);
+
+var x=15;
+x+=5;
+console.log(x);
+
+var r=35;
+r-=5;
+
+console.log(r);
+
+var s= 10;
+s*7;
+console.log(s);
+
+var t= 200;
+s/7;
+console.log(t);
+
+var u=10;
+u%=5;
+console.log(u);
+
+var num1=100;
+var num2=10;
+
+console.log(num1>num2);
+
+var num3 = 1;
+var num4=2;
+
+console.log(num3>num4);
+
+var num5 = 123;
+var num6="123";
+console.log(num5 === num6);
+
+var school='digital school'
+console.log(`hello from digital ${school}`)
