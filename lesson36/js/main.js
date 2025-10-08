b3bac60bde5f75d4a5619c6a273fe8c50ee09463
Mrock7 <guri.morina1@gmail.com>
2025-10-08 17:44:35 +0200

    m

---
+++ lesson36/js/main.js
@@ -0,0 +1,16 @@
+$(window).ready(function() {
+    alert('this is working');
+});
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForme :input');
+$('div:visible');
+$('div:gt(1)');
+$('div:animated');
+ 
